be5380b52584905cd21b2a6481b5b27d913784ef
Carlos Rios <apis@clickdata.pe>
2021-04-28 03:00:18 -0500

    Update

--- src/BackConfig.js
+++ src/BackConfig.js
@@ -1,4 +1,4 @@
-export const URL_BACK = 'https://evisorpressto.herokuapp.com/https://apiregistercpe.herokuapp.com/adminapp/';
+export const URL_BACK = 'https://apiregistercpe.herokuapp.com/adminapp/';
 // export const URL_BACK = 'http://127.0.0.1:8000/API/';
 
 export const FetchGet = async (endpoint) => {
